47bd205ec578f2299e32448cb537cc475a6df5fd
Joe Filippazzo <jfilippazzo@stsci.edu>
2026-05-08 15:21:58 -0400

    Bumped to v0.1.11

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hover2pi/hotsoss',
-    version='0.1.10',
+    version='0.1.11',
     zip_safe=False,
 )
